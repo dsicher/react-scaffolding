693dc627e0521994bbb722dffb40b049f7b4226a
davidsicher <dsicher@hugeinc.com>
2016-09-30 14:41:04 -0700

    simple state example added

--- src/js/components/HelloWorld.jsx
+++ src/js/components/HelloWorld.jsx
@@ -3,10 +3,23 @@ import React, { Component } from 'react';
 import shallowCompare from 'react-addons-shallow-compare';
 
 export default class HelloWorld extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    }
+  }
+
+  incrementCounter = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  }
+
   render() {
     return (
-      <div id={`hw`}>
-        "Hello World"
+      <div id={`hw`} style={{position: 'absolute', top: 0, left: 0, width: '1920px', height: '1080px'}} onClick={this.incrementCounter}>
+        "Hello World"<br />{this.state.counter}
       </div>
     )
   }
